1a95a893e88874bfab923375093a9482e7aeacac
Raban Gießler <63072611+Alorcus@users.noreply.github.com>
2025-10-23 15:47:51 +0200

    Query abstraction (#9)
    * initial structure
    
    * extend query structure
    
    * add Query Engine + tests
    
    * add query engine, operators, parser + simple tests
    
    * minor change (typo, class in quotes)
    
    * added phrase operator
    
    * testimng cicd, sorry
    
    * reverting fail, cicd succesfull
    
    ---------
    
    Co-authored-by: Konrad Goldenbaum <83073250+konradgold@users.noreply.github.com>
    Co-authored-by: Konrad Goldenbaum <konrad.goldenbaum@outlook.de>

---
+++ query_engine.py
@@ -0,0 +1,24 @@
+from query_operators import AbstractOperator
+from query_parser import Operators
+
+
+class QueryEngine:
+    def _process_phrase2query(self, phrase : str) -> AbstractOperator:
+        '''
+        Process the query and return the root operator
+        '''
+        phrase_elements = phrase.lower().split(" ")
+
+        parsing_order = [Operators.PHRASE, Operators.TERM, Operators.AND, Operators.ANDNOT,  Operators.OR]   
+        for current_operator in parsing_order:
+            idx = 0
+            while len(phrase_elements) > idx:
+                element = phrase_elements[idx]
+                if isinstance(element, AbstractOperator):
+                    idx += 1
+                    continue
+                clazz = Operators.get_EnumOperator(element)
+                if clazz == current_operator:
+                    phrase_elements = clazz(idx, phrase_elements)              
+                idx += 1
+        return phrase_elements[0]
---
+++ test_queries.py
@@ -0,0 +1,65 @@
+import unittest
+
+from regex import P
+
+from query_engine import QueryEngine
+from query_operators import ANDNOTOperator, ANDOperator, OROperator, PhraseOperator, TermOperator
+from query_parser import Operators
+
+
+class TestQueryEngine(unittest.TestCase):
+    def test_process_OR_query(self):
+        engine = QueryEngine()
+        root_operator = engine._process_phrase2query("a OR b")
+        self.assertIsInstance(root_operator, OROperator)
+        self.assertEqual(len(root_operator.children), 2)
+        self.assertIsInstance(root_operator.children[0], TermOperator)
+        self.assertIsInstance(root_operator.children[1], TermOperator)
+
+    def test_process_AND_query(self):
+        engine = QueryEngine()
+        root_operator = engine._process_phrase2query("a AND a ANDNOT b")
+        self.assertIsInstance(root_operator, ANDNOTOperator)
+        self.assertEqual(len(root_operator.children), 2)
+        self.assertIsInstance(root_operator.children[0], ANDOperator)
+        self.assertIsInstance(root_operator.children[1], TermOperator)
+        self.assertIsInstance(root_operator.children[0].children[0], TermOperator)
+        self.assertIsInstance(root_operator.children[0].children[1], TermOperator)
+
+    def test_process_PHRASE_query(self):
+        engine = QueryEngine()
+        root_operator = engine._process_phrase2query("'hello world' AND 'foo bar'")
+        self.assertIsInstance(root_operator, ANDOperator)
+        self.assertEqual(len(root_operator.children), 2)
+        self.assertIsInstance(root_operator.children[0], PhraseOperator)
+        self.assertIsInstance(root_operator.children[1], PhraseOperator)
+        self.assertEqual(root_operator.children[0].phrase, "'hello world'")
+        self.assertEqual(root_operator.children[1].phrase, "'foo bar'")
+
+    def test_process_PHRASE_single_tick(self):
+        engine = QueryEngine()
+        root_operator = engine._process_phrase2query("it's a test")
+        self.assertIsInstance(root_operator, TermOperator)
+        self.assertEqual(root_operator.phrase, "it's")
+
+    def test_operator_identification(self):
+        operator_class = Operators.get_EnumOperator("AND")
+        self.assertEqual(operator_class, Operators.AND) 
+        operator_class = Operators.get_EnumOperator("or")
+        self.assertEqual(operator_class, Operators.OR)
+        operator_class = Operators.get_EnumOperator("andnot")
+        self.assertEqual(operator_class, Operators.ANDNOT)
+        operator_class = Operators.get_EnumOperator("c.a.t")
+        self.assertEqual(operator_class, Operators.TERM)
+
+
+    def test_DEMO_for_execution(self):
+        engine = QueryEngine()
+        root_operator = engine._process_phrase2query("banana AND banana OR cherry ANDNOT banana")
+        result = root_operator.execute()
+        expected_ids = {"ID-banana", "ID-cherry"}
+        self.assertEqual(result, expected_ids)
+
+
+if __name__ == '__main__':
+    unittest.main()
